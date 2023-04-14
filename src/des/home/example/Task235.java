52112e2bdefa7631720c8db3ba26eb279596617e
Leonid <kissmykrabeskill@gmail.com>
2023-04-14 22:17:18 +0500

    todo

--- src/des/home/example/Task235.java
+++ src/des/home/example/Task235.java
@@ -12,6 +12,29 @@ public class Task235 {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        TreeNode desiredNode = root;
+        byte leftOrRight;
+
+        while (desiredNode != null) {
+            if (desiredNode.val == p.val || desiredNode.val == q.val) {
+                return desiredNode;
+            }
+
+//TODO
+            if (desiredNode.left.val == p.val || desiredNode.left.val == q.val || desiredNode.right.val == p.val || desiredNode.right.val == q.val) {
+                return desiredNode;
+            }
+
+            if (desiredNode.val > p.val && desiredNode.val > q.val) {
+                leftOrRight = 0;
+            } else leftOrRight = 1;
+
+            if (leftOrRight == 0) {
+                desiredNode = desiredNode.left;
+            } else {
+                desiredNode = desiredNode.right;
+            }
+        }
+        return null;
     }
 }
